6150a51a64f92410167259a410a8122ae00f7573
jkwcp <jk.cpwang@gmail.com>
2019-10-08 23:44:30 -0700

    finished lab3

--- js/addUser.js
+++ js/addUser.js
@@ -1,23 +1,108 @@
+init = () => {
+    document.getElementById("add-artist-submit").addEventListener('click', addArtist);
+    document.getElementById("triggerAddArtist").addEventListener('click', toggleArtistForm);
+    document.getElementById("searchArtist").addEventListener('click', searchArtist);
+    let artistList = readArtistList();
+    showArtistLists(artistList);
+}
+
+showArtistLists = (artistList) => {
+    for (let i = 0; i < artistList.length; i++) {
+        let artist = artistList[i];
+        let newNode = createArtistCard(artist.name, artist.description, artist.image);
+        document.getElementById("artistList").appendChild(newNode);
+    }
+}
+
+readArtistList = () => {
+    var artistList = JSON.parse(localStorage.getItem("artistList"));
+    if (artistList == null) {
+        artistList = [];
+    } else if (artistList.length === undefined) {
+        artistList = [artistList];
+    }
+    return artistList;
+}
+
+addToArtistList = (name, description, image) => {
+    let artist = makeArtist(name,description, image);
+    let artistList = readArtistList();
+    artistList.push(artist);
+    setArtistList(artistList);
+}
+
+removeFromArtistList = (name) => {
+    let artistList = readArtistList();
+    
+    for (let i = 0; i < artistList.length; i++) {
+        let artist = artistList[i];
+        if (artist.name === name) {
+            artistList.splice(i, 1);
+            i--;
+        }
+    }
+    setArtistList(artistList);
+}
+
+searchFromArtistList = (name) => {
+    let artistList = readArtistList();
+    let searchResult = [];
+
+    for (let i = 0; i < artistList.length; i++) {
+        let artist = artistList[i];
+        if (artist.name.includes(name)) {
+            searchResult.push(artist);
+        }
+    }
+
+    return searchResult;
+}
+
+setArtistList = (artistList) => {
+    localStorage.setItem("artistList", JSON.stringify(artistList));
+}
+
+makeArtist = (name, description, image) => {
+    return {
+        name: name,
+        description: description,
+        image: image
+    };
+}
+
+function searchArtist() {
+    let name = document.getElementById("search-artist").value;
+    if (name === "" || name === null) return;
+    let artistPanel = document.getElementById("artistList");
+    while (artistPanel.firstChild) {
+        artistPanel.removeChild(artistPanel.firstChild);
+    }
+    let result = searchFromArtistList(name);
+    showArtistLists(result);
+
+}
+
 function addArtist() {
-    const name = document.getElementById("add_artist_name").value;
-    const description = document.getElementById("add_artist_description").value;
-    const image = document.getElementById("add_artist_image").value;
+    const name = document.getElementById("add-artist-name").value;
+    const description = document.getElementById("add-artist-description").value;
+    const image = document.getElementById("add-artist-image").value;
 
     if (name === "" || description === "" || image === "") {
         return;
     }
 
     let newNode = createArtistCard(name, description, image);
-
     document.getElementById("artistList").appendChild(newNode);
+    addToArtistList(name,description,image);
     clearForm();
-
 }
 
+
+
 clearForm = () => {
-    document.getElementById("add_artist_name").value="";
-    document.getElementById("add_artist_description").value="";
-    document.getElementById("add_artist_image").value="";
+    document.getElementById("add-artist-name").value="";
+    document.getElementById("add-artist-description").value="";
+    document.getElementById("add-artist-image").value="";
     document.getElementById("add-artist-panel").style.display = "none";
 }
 
@@ -77,6 +162,8 @@ function createArtistCard(name,description, url) {
 
 function deleteArtist(e) {
     const artistDiv = e.target.parentElement.parentElement;
+    let name = artistDiv.querySelector(".name").innerText;
+    removeFromArtistList(name);
     artistDiv.parentElement.removeChild(artistDiv);
 }
 
